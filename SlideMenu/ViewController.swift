ba251c8063ff459d5704e0acbc055890448f56fe
janarthan s <janathanreddy@gmail.com>
2021-01-01 22:42:45 +0530

    Slide Menu Transition Done Menu Option Choose Dismiss Menu Change Color and Animate Done

--- SlideMenu/ViewController.swift
+++ SlideMenu/ViewController.swift
@@ -8,12 +8,66 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let transiton = SlideInTransition()
+    var topView: UIView?
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func SlideMenu(_ sender: Any) {
+        guard let menuViewController = storyboard?.instantiateViewController(withIdentifier: "MenuViewController") as? MenuViewController else { return }
+        menuViewController.didTapMenuType = { menuType in
+            self.transitionToNew(menuType)
+        }
+        menuViewController.modalPresentationStyle = .overCurrentContext
+        menuViewController.transitioningDelegate = self
+        present(menuViewController, animated: true)
+    }
+    func transitionToNew(_ menuType: MenuType) {
+        let title = String(describing: menuType).capitalized
+        self.title = title
 
+        topView?.removeFromSuperview()
+        switch menuType {
+        case .profile:
+            let view = UIView()
+            view.backgroundColor = .yellow
+            view.frame = self.view.bounds
+            self.view.addSubview(view)
+            self.topView = view
+        case .products:
+            let view = UIView()
+            view.backgroundColor = .blue
+            view.frame = self.view.bounds
+            self.view.addSubview(view)
+            self.topView = view
+        case .settings:
+            let view = UIView()
+            view.backgroundColor = .systemPink
+            view.frame = self.view.bounds
+            self.view.addSubview(view)
+            self.topView = view
+        case .logout:
+            let view = UIView()
+            view.backgroundColor = .systemIndigo
+            view.frame = self.view.bounds
+            self.view.addSubview(view)
+            self.topView = view
+        default:
+            break
+        }
+    }
+
+}
+extension ViewController: UIViewControllerTransitioningDelegate {
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        transiton.isPresenting = true
+        return transiton
+    }
+
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        transiton.isPresenting = false
+        return transiton
+    }
 }
 
